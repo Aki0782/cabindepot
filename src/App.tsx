a21db5640b2cbfb873abc5c7a246e6aa44eaf72c
Akhil Aravindakshan <akhil.dev.00782@gmail.com>
2024-06-27 15:14:24 -0300

    Orders page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Landing from "./pages/Landing";
+import Orders from "./pages/Orders/Orders";
 import Overview from "./pages/Overview";
 
 const App: React.FC = () => {
@@ -10,7 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Landing />}>
           <Route path="" element={<Overview />} />
-          <Route path="orders" element={<Overview />} />
+          <Route path="orders" element={<Orders />} />
           <Route path="invoices" element={<Overview />} />
           <Route path="contacts" element={<Overview />} />
           <Route path="assets" element={<Overview />} />
